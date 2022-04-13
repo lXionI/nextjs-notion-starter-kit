00ecd40c005bd4608fa988816de56d5fac8db4f1
armetuer <merih.tuerkoglu@stud.hs-ruhrwest.de>
2022-04-13 11:21:14 +0200

    Notion root changed again

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1d52c17e89f541c5904eb02e6a3e62dc',
+  rootNotionPageId: 'b1a9453a3c06454d97eab3ba6bffc8b1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
